d83fab9db9d6f7fd6ab84f13865642e3e327283d
flores-97 <floresvictor15@gmail.com>
2025-04-08 16:59:51 -0700

    Add project files.

---
+++ COMP003B.MiniProject/Controllers/GameSystemController.cs
@@ -0,0 +1,24 @@
+﻿using COMP003B.MiniProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.MiniProject.Controllers
+{
+    public class GameSystemController : Controller
+    {
+        public IActionResult Index()
+        {
+            var gameSystems = new List<GameSystem>
+            {
+                new GameSystem { Id = 1, Platform = "Sony PlayStation", Summary = "Power to the player." },
+                new GameSystem { Id = 2, Platform = "Microsoft Xbox", Summary = "This is Xbox." }
+            };
+
+            return View(gameSystems);
+        }
+        public IActionResult Details(int id)
+        {
+            var gameSystem = new GameSystem { Id = id, Platform = $"Platform {id}", Summary = "Detailed content coming soon." };
+            return View(gameSystem);
+        }
+    }
+}
---
+++ COMP003B.MiniProject/Models/GameSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.MiniProject.Models
+{
+    public class GameSystem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Platform { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Summary { get; set; }
+
+        [Range(100, 1000)]
+        public decimal Price { get; set; }
+
+        [Range(100, 1000)]
+        public decimal StorageGB { get; set; }
+    }
+}
