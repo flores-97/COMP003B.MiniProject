78c0969b4245773ec31b9e08242fc02a740e49e2
flores-97 <floresvictor15@gmail.com>
2025-04-08 18:55:56 -0700

    changed welcome wepage and added minor styling

--- COMP003B.MiniProject/Controllers/GameSystemController.cs
+++ COMP003B.MiniProject/Controllers/GameSystemController.cs
@@ -9,8 +9,9 @@ namespace COMP003B.MiniProject.Controllers
         {
             var gameSystems = new List<GameSystem>
             {
-                new GameSystem { Id = 1, Platform = "Sony PlayStation", Summary = "Power to the player." },
-                new GameSystem { Id = 2, Platform = "Microsoft Xbox", Summary = "This is Xbox." }
+                new GameSystem { Id = 1, Platform = "Sony PlayStation", Summary = "Power to the player. Sony has been a fan favorite and the players choice, and highly saught after.", Price = 400, StorageGB = 1 },
+                new GameSystem { Id = 2, Platform = "Microsoft Xbox", Summary = "This is Xbox. Xbox has had loyal players, especially due to it's exclusives. The Halo games keep players coming back.", Price = 500, StorageGB = 1},
+                new GameSystem { Id = 3, Platform = "Computer Gaming", Summary = "Build your PC to your liking. You can have the specs you want and the price range you can afford. Whether you want high end or medium, the choice is yours.", Price = 500, StorageGB = 1}
             };
 
             return View(gameSystems);
