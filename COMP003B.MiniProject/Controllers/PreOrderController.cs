8cd9ece071813845548d2f3cade088e6470108c4
flores-97 <floresvictor15@gmail.com>
2025-04-09 14:09:44 -0700

    new model, controller and view created for form.

---
+++ COMP003B.MiniProject/Controllers/PreOrderController.cs
@@ -0,0 +1,37 @@
+﻿using COMP003B.MiniProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.MiniProject.Controllers
+{
+    public class PreOrderController : Controller
+    {
+        [HttpGet("preorder/{platform}")]
+        public IActionResult PreOrder([FromForm] string preOrder)
+        {
+            var model = new PreOrder { Platform = preOrder };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult PreOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PreOrder([FromForm] PreOrder model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Success", model);
+        }
+
+        [HttpGet]
+        public IActionResult Success(PreOrder model)
+        {
+            return View(model); 
+        }
+    }
+}
---
+++ COMP003B.MiniProject/Models/PreOrder.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.MiniProject.Models
+{
+    public class PreOrder
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        [Range(1, 3, ErrorMessage = "Max limit per customer is 3.")]
+        public int OrderUnit { get; set; }
+
+        public string? Discount { get; set; }
+    }
+}
